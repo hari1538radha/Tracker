1da77c4a36cba0c3b6271858e5ce8a0efb70a16f
hari1538radha <hari.r2020ece@sece.ac.in>
2022-10-22 18:36:52 +0530

    completed login page ui without footer component

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -7,9 +7,12 @@ const Login = () => {
     <div>
       <Navbar />
       <div className="relative font-poppins">
-        <img className="bg-cover" src={Background}></img>
+        <img className="bg-cover w-[100%]" src={Background}></img>
         <div className="  flex flex-col  items-center bg-white absolute  rounded-xl gap-y-4 w-[410px] left-1/3 top-[89px]">
-          <h1 className="text-3x mt-51 font-bold text-[42px] font-sans"> Log In</h1>
+          <h1 className="text-3x mt-51 font-bold text-[42px] font-sans">
+            {" "}
+            Log In
+          </h1>
           <input
             className="w-[330px] border-2 border-grey-300 pl-[20px] bg-white h-51 rounded-[100px] mt-[45px]"
             type="text"
@@ -20,12 +23,14 @@ const Login = () => {
             className="bg-white h-51 w-[330px] rounded-[100px] pl-[20px] border-2 border-grey-300 mt-[20px]"
             type="password"
             placeholder="Your password"
-            required
+            required="true"
           ></input>
           <button className="bg-[#6496EB] text-white w-[330px] h-51 mt-[40px] rounded-full ">
             Log In
           </button>
-          <span className="text-[#F75B46] mt-[20px] mb-51 ">Invalid Credentials</span>
+          <span className="text-[#F75B46] mt-[20px] mb-51 ">
+            Invalid Credentials
+          </span>
         </div>
       </div>
     </div>
